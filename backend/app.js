f1fe3bf1cde784e488cdbeeec3f9f259a7b4491d
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-21 11:47:10 +0100

    feat(backend): Testing creation of first route

--- backend/app.js
+++ backend/app.js
@@ -2,22 +2,48 @@ const express = require('express');
 
 const app = express();
 
+// No route specified because we want the middleware to be appplied to every routes of the server
 app.use((request, response, next) => {
-    console.log('Request received.');
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
-app.use((request, response, next) => {
-    response.status(201);
-    next();
-});
-
-app.use((request, response, next) => {
-    response.json({ message: 'Request received.'});
-    next();
-});
-
-app.use((request, response) => {
+app.use('/api/books', (request, response, next) => {
+    const books = [
+        {
+            userId : 'shifldhjfjHJKHH',
+            title : 'Titre du Livre',
+            author : 'Super Auteur',
+            imageUrl : '#',
+            year: '2024',
+            genre: 'genre',
+            ratings : [
+            {
+            userId : 'shifldhjfj367BG',
+            grade : 4
+            }
+            ], 
+            averageRating : null
+        },
+        {
+            userId : 'shifldhjfj367BG',
+            title : 'Super Titre',
+            author : 'Super Auteur',
+            imageUrl : '#',
+            year: '2022',
+            genre: 'genre',
+            ratings : [
+            {
+            userId : 'shifldhjfjHJKHH',
+            grade : 3
+            }
+            ], 
+            averageRating : null
+        }
+    ];
+    response.status(200).json(books);
     console.log('Response successfully sent !');
 });
 
