a3d41172c46621efb117bc0f215b6fcf440ec749
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-21 15:26:34 +0100

    feat(backend): Add mongoose connection to mongoDB

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
+mongoose.connect('mongodb+srv://dbUser:FBQa6SLTYNbQmEzl@cluster0.ijofa9c.mongodb.net/',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
 // new way, old way was "body-parser"
 // allow the use of request.body
-// password: FBQa6SLTYNbQmEzl
 app.use(express.json());
 
 // No route specified because we want the middleware to be appplied to every routes of the server
