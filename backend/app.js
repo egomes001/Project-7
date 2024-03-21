5f2c9ee2492575a597644a3214dc085d6bd15aff
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-21 11:12:35 +0100

    feat(backend): Add first middlewares of the app + port standardisation and error handling in server.js

--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,23 @@ const express = require('express');
 
 const app = express();
 
+app.use((request, response, next) => {
+    console.log('Request received.');
+    next();
+});
+
+app.use((request, response, next) => {
+    response.status(201);
+    next();
+});
+
+app.use((request, response, next) => {
+    response.json({ message: 'Request received.'});
+    next();
+});
+
+app.use((request, response) => {
+    console.log('Response successfully sent !');
+});
+
 module.exports = app;
