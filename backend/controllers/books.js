008adeda98cf6f4fd4e361e7118bd7132dd5cbe0
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-28 10:44:15 +0100

    feat: Separated controller logic from route logic

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,5 +1,17 @@
 const Book = require('../models/Book');
 
+/**
+ * retrieve all books
+ */
+exports.getBooks = (request, response, next) => {
+    Book.find()
+     .then(books => response.status(200).json(books))
+     .catch(error => response.status(400).json({ error }));
+};
+
+/**
+ * manage individual book
+ */
 exports.createBook = (request, response, next) => {
     delete request.body._id;
     const book = new Book({
@@ -8,4 +20,22 @@ exports.createBook = (request, response, next) => {
     book.save()
     .then(() => response.status(201).json({ message: 'Book saved !'}))
     .catch(error => response.status(400).json({ error }));
+};
+
+exports.editBook = (request, response, next) => {
+    Book.updateOne({ _id: request.params.id }, { ...request.body, _id: request.params.id })
+     .then(() => response.status(200).json({ message: 'Book modified !' }))
+     .catch(error => response.status(400).json({ error })); 
+};
+
+exports.deleteBook = (request, response, next) => {
+    Book.deleteOne({ _id: request.params.id })
+     .then(() => response.status(200).json({ message: 'Book deleted !' }))
+     .catch(error => response.status(400).json({ error }));
+};
+
+exports.getOneBook = (request, response, next) => {
+    Book.findOne({ _id: request.params.id })
+     .then(book => response.status(200).json(book))
+     .catch(error => response.status(404).json({ error }));
 };
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -4,32 +4,13 @@ const router = express.Router();
 const bookController = require('../controllers/books');
 
 router.post('/', bookController.createBook);
+router.get('/', bookController.getBooks);
 
-router.get('/:id', (request, response, next) => {
-    Book.findOne({ _id: request.params.id })
-     .then(book => response.status(200).json(book))
-     .catch(error => response.status(404).json({ error }));
-    next();
-});
+router.get('/:id', bookController.getOneBook);
 
-router.put('/:id', (request, response, next) => {
-    Book.updateOne({ _id: request.params.id }, { ...request.body, _id: request.params.id })
-     .then(() => response.status(200).json({ message: 'Book modified !' }))
-     .catch(error => response.status(400).json({ error })); 
-    next();
-});
 
-router.delete('/:id', (request, response, next) => {
-    Book.deleteOne({ _id: request.params.id })
-     .then(() => response.status(200).json({ message: 'Book deleted !' }))
-     .catch(error => response.status(400).json({ error }));
-    next();
-});
+router.put('/:id', bookController.editBook);
 
-router.get('/', (request, response, next) => {
-    Book.find()
-     .then(books => response.status(200).json(books))
-     .catch(error => response.status(400).json({ error }));
-});
+router.delete('/:id', bookController.deleteBook);
 
 module.exports = router;
