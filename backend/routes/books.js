c404353342a259719a198fa0f51a84f1cbdb7f33
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-28 10:56:50 +0100

    refactor: Formatting code for more clarity

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -7,10 +7,7 @@ router.post('/', bookController.createBook);
 router.get('/', bookController.getAllBooks);
 
 router.get('/:id', bookController.getOneBook);
-
-
 router.put('/:id', bookController.editBook);
-
 router.delete('/:id', bookController.deleteBook);
 
 module.exports = router;
