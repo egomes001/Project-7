f1dd52828e82bb338306cd992343450fc9ba9f17
Julianne Duhaze <jduhaze@protonmail.com>
2024-03-29 10:15:12 +0100

    fix(backend): Fixed typo (header instead of headers)

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (request, response, next) => {
     try {
-        const token = request.header.authorization.split(' ')[1];
+        const token = request.headers.authorization.split(' ')[1];
         const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
         const userId = decodedToken.userId;
         request.auth = {
